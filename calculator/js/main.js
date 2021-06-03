3bca07f64befb179f40002bae69fbcd284df4e2e
Kemil <kemilbeltre@gmail.com>
2021-06-03 14:09:44 +0100

    Calculator with the essentials functionalities

---
+++ calculator/js/main.js
@@ -0,0 +1,53 @@
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+const display = calculator.querySelector(".calculator__display");
+
+keys.addEventListener("click", (e) => {
+  if (!e.target.closest("button")) return;
+
+  const key = e.target;
+  const keyValue = key.textContent;
+  const displayValue = display.textContent;
+  const { type } = key.dataset;
+  const { previusKeyType } = calculator.dataset;
+
+  if (type === "number") {
+    if (displayValue === "0" || previusKeyType === "operator") {
+      display.textContent = keyValue;
+    } else {
+      display.textContent = displayValue + keyValue;
+    }
+  }
+
+  if (type === "operator") {
+    const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
+    operatorKeys.forEach((e) => {
+      e.dataset.state = "";
+    });
+    key.dataset.state = "selected";
+
+    calculator.dataset.firstNumber = displayValue;
+    calculator.dataset.operator = key.dataset.key;
+  }
+
+  if (type === "equal") {
+    const firstNumber = calculator.dataset.firstNumber;
+    const operator = calculator.dataset.operator;
+    const secondNumber = displayValue;
+
+    display.textContent = calculate(firstNumber, operator, secondNumber);
+  }
+  calculator.dataset.previusKeyType = type;
+});
+
+function calculate(firstNumber, operator, secondNumber) {
+  firstNumber = parseInt(firstNumber);
+  secondNumber = parseInt(secondNumber);
+
+  let result = "";
+  if (operator === "plus") result = firstNumber + secondNumber;
+  if (operator === "min") result = firstNumber - secondNumber;
+  if (operator === "divide") result = firstNumber / secondNumber;
+  if (operator === "times") result = firstNumber * secondNumber;
+  return result.toFixed(2);
+}
